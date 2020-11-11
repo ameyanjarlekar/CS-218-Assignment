91dbf4a422659905ec3b5ad49fc09d902a9bf464
ameyanjarlekar <40833548+ameyanjarlekar@users.noreply.github.com>
2020-11-11 23:30:18 +0530

    Update dpcode.c

--- dpcode.c
+++ dpcode.c
@@ -4,28 +4,28 @@
 using namespace std;
 
 int main() {
-    int m,E,hcost,fcost,S,C,OTC,OTprice,W;
-    cin>>m;
+    int m,E,hcost,fcost,S,C,OTC,OTprice,W; //declare variables
+    cin>>m;                                 //take input
     int D[m];
     int maxval=0;
     for(int i=0;i<m;i++)
     {   
-        cin>>D[i];
-        maxval = max(maxval,D[i]);
+        cin>>D[i];                      // take input 
+        maxval = max(maxval,D[i]);      
     }
     cin>>E>>hcost>>fcost;
     cin>>S>>C;
     maxval = max(maxval,E);
-    int lim = maxval/C+2;
+    int lim = maxval/C+2;               // upper limit on number of workers daily
     cin>>OTC>>OTprice;
     cin>>W;
-    int Cost[m+1][lim][lim*OTC][2];
+    int Cost[m+1][lim][lim*OTC][2];     // dim 1 cost upto day i, dim 2 no of workers used on day i, dim 3 no of overtime hours on day i, dim 4 first element is cost upto day i second is excess carpets produced on day i
     for(int i=0;i<lim;i++)
     {
         for(int j=0;j<lim*OTC;j++)
         {
-            Cost[0][i][j][0]=0;
-            Cost[0][i][j][1]=0;
+            Cost[0][i][j][0]=0;        // initialize cost of day 0
+            Cost[0][i][j][1]=0;         // initialize excess carpets for day 0
         }
     }
     
@@ -38,50 +38,50 @@ int main() {
             {   Cost[day][work][over][0]=-1;
                  if(day == 1)
                     {int prevwork = E;
-                    int prevover=0;
+                    int prevover=0;             // for day 1 we had E workers initally and none did overtime
                        refer = Cost[day][work][over][0];
-                        refer2 = Cost[day-1][prevwork][prevover][1]-D[day-1]+over+C*work;
-                        if(work>=prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1)
+                        refer2 = Cost[day-1][prevwork][prevover][1]-D[day-1]+over+C*work;       // excess carpets produced
+                        if(work>=prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1) // consider if excess hours >=0
                         {
                         if(Cost[day][work][over][0]==-1)
-                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(work-prevwork)*hcost+Cost[day-1][0][0][1]*W+over*OTprice;
+                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(work-prevwork)*hcost+Cost[day-1][0][0][1]*W+over*OTprice; // cost = prev cost + S*workers+increase in workers * hire cost + storage cost for excess produced last time + overtime*OTcost
                         else
                         Cost[day][work][over][0] = min(Cost[day-1][prevwork][prevover][0]+S*work+(work-prevwork)*hcost+Cost[day-1][0][0][1]*W+over*OTprice,Cost[day][work][over][0]);
                         }
-                        else if (work<prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1)
+                        else if (work<prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1)  // consider if excess hours >=0
                         {
                         if(Cost[day][work][over][0]==-1)
-                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(-work+prevwork)*fcost+Cost[day-1][0][0][1]*W+over*OTprice;
+                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(-work+prevwork)*fcost+Cost[day-1][0][0][1]*W+over*OTprice; // cost = prev cost + S*workers+decrease in workers * fire cost + storage cost for excess produced last time + overtime*OTcost
                         else
                         Cost[day][work][over][0] = min(Cost[day-1][prevwork][prevover][0]+S*work+(-work+prevwork)*fcost+Cost[day-1][0][0][1]*W+over*OTprice,Cost[day][work][over][0]);
                         }                        
                         if (Cost[day][work][over][0]!=0)
-                        Cost[day][work][over][1]=refer2;
+                        Cost[day][work][over][1]=refer2;        // update excess carpets
                     
                     }
                 else
                 {                        Cost[day][work][over][1]=-1;
-                for(int prevwork=0;prevwork<lim;prevwork++)
+                for(int prevwork=0;prevwork<lim;prevwork++) // prev workers could range from 0 to lim for day>1
                 {  
-                    for(int prevover=0;prevover<=prevwork*OTC;prevover++)
+                    for(int prevover=0;prevover<=prevwork*OTC;prevover++)  // over time carpets could range from 0 to workers*OTC for day>1
                     {   refer = Cost[day][work][over][0];
-                        refer2 = Cost[day-1][prevwork][prevover][1]-D[day-1]+over+C*work;
-                        if(work>=prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1)
+                        refer2 = Cost[day-1][prevwork][prevover][1]-D[day-1]+over+C*work;   // calc excess hours
+                        if(work>=prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1) // consider if excess hours >=0
                         {
                         if(Cost[day][work][over][0]==-1)
-                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(work-prevwork)*hcost+Cost[day-1][prevwork][prevover][1]*W+over*OTprice;
+                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(work-prevwork)*hcost+Cost[day-1][prevwork][prevover][1]*W+over*OTprice;      // cost = prev cost + S*workers+increase in workers * hire cost + storage cost for excess produced last time + overtime*OTcost
                         else
                         Cost[day][work][over][0] = min(Cost[day-1][prevwork][prevover][0]+S*work+(work-prevwork)*hcost+Cost[day-1][prevwork][prevover][1]*W+over*OTprice,Cost[day][work][over][0]);
                         }
-                        else if (work<prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1)
+                        else if (work<prevwork && refer2>=0 && Cost[day-1][prevwork][prevover][0] != -1) // consider if excess hours >=0
                         {
                         if(Cost[day][work][over][0]==-1)
-                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(-work+prevwork)*fcost+Cost[day-1][prevwork][prevover][1]*W+over*OTprice;
+                        Cost[day][work][over][0] = Cost[day-1][prevwork][prevover][0]+S*work+(-work+prevwork)*fcost+Cost[day-1][prevwork][prevover][1]*W+over*OTprice;  // cost = prev cost + S*workers+decrease in workers * fire cost + storage cost for excess produced last time + overtime*OTcost
                         else
                         Cost[day][work][over][0] = min(Cost[day-1][prevwork][prevover][0]+S*work+(-work+prevwork)*fcost+Cost[day-1][prevwork][prevover][1]*W+over*OTprice,Cost[day][work][over][0]);
                         }                        
                         if (refer>Cost[day][work][over][0] || (refer == -1 && Cost[day][work][over][0] != -1))
-                        Cost[day][work][over][1]=refer2;
+                        Cost[day][work][over][1]=refer2;  // update excess carpets
                     }
                 }
                 }
@@ -96,9 +96,9 @@ int main() {
             if(Cost[m][i][j][0]!=-1)
             {if (mincost != -1)
             {   if(i>=E)
-                mincost = min(mincost,Cost[m][i][j][0]+(i-E)*fcost);
+                mincost = min(mincost,Cost[m][i][j][0]+(i-E)*fcost);  // fire workers to make final workers = E
                 else
-                mincost = min(mincost,Cost[m][i][j][0]+(E-i)*hcost);
+                mincost = min(mincost,Cost[m][i][j][0]+(E-i)*hcost);     // hire workers to make final workers = E
             }
             else
             {   if(i>=E)
@@ -107,36 +107,10 @@ int main() {
                 mincost = Cost[m][i][j][0]+(E-i)*hcost;
             }
             }
-           // cout<<Cost[1][i][j][0]<<" ";
         }
-        //cout<<endl;
         }
- /*       for(int i=0;i<lim;i++)
-        {
-        for(int j=0;j<=i;j++)
-        {  
-            cout<<Cost[1][i][j][1]<<" ";
-        }
-        cout<<endl;
-        }
-        for(int i=0;i<lim;i++)
-        {
-        for(int j=0;j<=i;j++)
-        {  
-            cout<<Cost[2][i][j][0]<<" ";
-        }
-        cout<<endl;
-        }
-        for(int i=0;i<lim;i++)
-        {
-        for(int j=0;j<=i;j++)
-        {  
-            cout<<Cost[2][i][j][1]<<" ";
-        }
-        cout<<endl;
-        }
-        */
-        cout<<mincost<<endl;
+
+        cout<<mincost<<endl;        // print optimal final answer
     
 }
 
